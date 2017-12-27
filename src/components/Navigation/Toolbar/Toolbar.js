65df79ec3d5d97a72a4a39e17cc11a3027942fcc
Fabio Kiatkowski <xolla@xolla.com.br>
2017-12-27 16:02:30 -0200

    Initial commit

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+
+import classes from './Toolbar.css';
+
+const toolbar = () => {
+  return (
+    <header className={classes.Toolbar}>
+      <div>MENU</div>
+      <Logo />
+      <nav>
+        <NavigationItems />
+      </nav>
+    </header>
+  );
+}
+
+export default toolbar;
---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Aux from '../../../hoc/Auxiliary';
+import Backdrop from '../Backdrop/Backdrop';
+import classes from './Modal.css';
+
+const modal = (props) => (
+  <Aux>
+    <Backdrop show={props.show} clicked={props.modalClosed} />
+    <div 
+      style={
+        {
+          transform: props.show ? 'translateY(0)' : 'translateY(-10vh)',
+          opacity: props.show ? '1': '0'
+        }
+      }
+      className={classes.Modal}>
+      {props.children}
+    </div>  
+  </Aux>
+);
+
+export default modal;
