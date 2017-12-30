8557f487e0362cab63c2548640f31a7ddcd7f9cc
Fabio Kiatkowski <xolla@xolla.com.br>
2017-12-30 15:23:38 -0200

    Added Firebase and Spinner feature.

---
+++ src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classses from './Spinner.css'
+
+const spinner = () => {
+  return (
+    <div className={classses.Loader}>Loading...</div>
+  );
+}
+
+export default spinner;
