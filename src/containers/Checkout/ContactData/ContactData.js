04f9e817871320ff67bcd7ac0d087cddd3863aec
Fabio Kiatkowski <xolla@xolla.com.br>
2018-01-21 09:36:42 -0200

    refactoring without test

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,6 +7,7 @@ import Input from '../../../components/UI/Input/Input';
 import axios from '../../../axios-orders';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/index';
+import { updateObject, checkValidity } from '../../../shared/utility';
 
 import classes from './ContactData.css';
 
@@ -135,49 +136,24 @@ class ContactData extends Component {
     this.props.onOrderBurger(order, this.props.tokenId);
   }
 
-  checkValidity(value, rules) {
-    let isValid = true;
-    if (!rules) {
-        return true;
-    }
-    
-    if (rules.required) {
-        isValid = value.trim() !== '' && isValid;
-    }
-
-    if (rules.minLength) {
-        isValid = value.length >= rules.minLength && isValid
-    }
-
-    if (rules.maxLength) {
-        isValid = value.length <= rules.maxLength && isValid
-    }
-
-    if (rules.isEmail) {
-        const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-        isValid = pattern.test(value) && isValid
-    }
-
-    if (rules.isNumeric) {
-        const pattern = /^\d+$/;
-        isValid = pattern.test(value) && isValid
-    }
-
-    return isValid;
-}
-
   inputChangedHandler = (event, inputIdentifier) => {
-    const formData = {
-      ...this.state.orderForm
-    }
-    const elementData = {
-      ...formData[inputIdentifier]
-    }
-    elementData.value = event.target.value;
-    elementData.valid.value = this.checkValidity(elementData.value, elementData.validation)
-    elementData.touched = true;
-    formData[inputIdentifier]=elementData;
-    
+    const updateValidValue = updateObject(
+      this.state.orderForm[inputIdentifier].valid, 
+      {
+        value: checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation)
+      }
+    );
+
+    const elementData = updateObject(this.state.orderForm[inputIdentifier], {
+      value: event.target.value,
+      valid: updateValidValue,
+      touched: true
+    });
+    const formData = updateObject(this.state.orderForm,
+    {
+      [inputIdentifier]: elementData
+    })
+
     let formIsValid = true;
     for (let i in formData) {
       formIsValid = formData[i].valid.value && formIsValid
